e5ec67cf8c573cd4a9b231d0db3799c6f857cf75
Felipe Dias <adfelipealmeida@gmail.com>
2023-01-02 17:01:02 -0300

    finalizado layout tela de detalhes de contato

--- lib/android/views/details.view.dart
+++ lib/android/views/details.view.dart
@@ -20,6 +20,158 @@ class DetailsView extends StatelessWidget {
           color: Colors.black, //altera a cor da trailing
         ),
       ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 10,
+            width: double.infinity,
+          ),
+          Container(
+            width: 200,
+            height: 200,
+            padding: const EdgeInsets.all(
+                10), // Adicionando esse espaçamento, aparece o efeito atrás da imagem
+            decoration: BoxDecoration(
+              color: Theme.of(context)
+                  .primaryColor
+                  .withOpacity(0.1), // opcidade vai sempre até "1"
+              borderRadius: BorderRadius.circular(200),
+            ),
+            child: Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                image: const DecorationImage(
+                  image: NetworkImage("https://picsum.photos/250?image=9"),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          const Text(
+            "Felipe Dias",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Text(
+            "14 99710-1478",
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          const Text(
+            "adfelipealmeida@mail.com",
+            style: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Container(
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Theme.of(context).primaryColor,
+                ),
+                child: TextButton(
+                  onPressed: () {},
+                  child: Icon(
+                    Icons.phone,
+                    color: Theme.of(context).colorScheme.secondary,
+                    size: 30,
+                  ),
+                ),
+              ),
+              Container(
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Theme.of(context).primaryColor,
+                ),
+                child: TextButton(
+                  onPressed: () {},
+                  child: Icon(
+                    Icons.email,
+                    color: Theme.of(context).colorScheme.secondary,
+                    size: 30,
+                  ),
+                ),
+              ),
+              Container(
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Theme.of(context).primaryColor,
+                ),
+                child: TextButton(
+                  onPressed: () {},
+                  child: Icon(
+                    Icons.camera_enhance,
+                    color: Theme.of(context).colorScheme.secondary,
+                    size: 30,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 40),
+          ListTile(
+            title: const Text(
+              "Endereço",
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 16,
+              ),
+            ),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  "Rua do Desenvolvedor, 256",
+                  style: TextStyle(
+                    fontSize: 12,
+                  ),
+                ),
+                Text(
+                  "Botucatu/SP",
+                  style: TextStyle(
+                    fontSize: 12,
+                  ),
+                ),
+              ],
+            ),
+            isThreeLine:
+                true, // Utilizando quando o Title ou Subtitle não é somente uma linha,
+            trailing: TextButton(
+              onPressed: () {},
+              child: Icon(
+                Icons.pin_drop,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Theme.of(context).primaryColor,
+        child: Icon(
+          Icons.edit,
+          color: Theme.of(context).colorScheme.secondary,
+        ),
+      ),
     );
   }
 }
