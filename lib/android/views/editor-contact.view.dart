f9f4519fe6521cbf54f4c909acbd993adeb27fb9
Felipe Dias <adfelipealmeida@gmail.com>
2023-01-12 16:47:30 -0300

    criado formulário e botão

--- lib/android/views/editor-contact.view.dart
+++ lib/android/views/editor-contact.view.dart
@@ -11,6 +11,56 @@ class EditorContactView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: model == null
+            ? const Text(
+                "Novo Contato",
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              )
+            : const Text(
+                "Editar Contato",
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        iconTheme: const IconThemeData(
+          color: Colors.black, //altera a cor da trailing
+        ),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                initialValue: model?.name,
+                onSaved: (value) => model?.name = value ?? '',
+              ),
+              TextFormField(
+                initialValue: model?.email,
+                onSaved: (value) => model?.email = value ?? '',
+              ),
+              TextFormField(
+                initialValue: model?.phone,
+                onSaved: (value) => model?.phone = value ?? '',
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                child: const Text("Salvar"),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
